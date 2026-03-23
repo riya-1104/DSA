2a5464f2d6f3e0a462e719a3d8f61e30efa6ed11
Riya <riyakarn1107@gmail.com>
2026-03-23 23:49:01 +0530

    Added max element problem

---
+++ arrays/max.cpp
@@ -0,0 +1,20 @@
+//max val out of all elements 
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter size of array=";
+	cin>>n;
+	int arr[n];
+    for(int i=0;i<n;i++){
+    cout<<"enter elements "<<i<<"=";
+   	cin>>arr[i];
+	}
+	int max=arr[0];
+	for(int i=1;i<n;i++){
+		if(max<arr[i]){
+			max=arr[i];
+		}
+	}
+	cout<<max;
+}
