ffbc0c29217b5102ed7aa49c603909e8e84a8d04
Riya <riyakarn1107@gmail.com>
2026-03-27 01:29:20 +0530

    Added string: count number of neighbouring characters that are different

---
+++ string/count_neighbour_differences.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int count = 0;
+
+    for(int i = 0; i < s.length() - 1; i++) {
+        if(s[i] != s[i + 1]) {
+            count++;
+        }
+    }
+
+    cout << count;
+    return 0;
+}
